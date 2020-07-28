13c7d78b8357bec02e0b1f08145e428e759c8d18
Rodrigo Campos <rodrigo.ccm@live.com>
2020-07-28 10:13:19 -0300

    commit on master

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const checkboxTwo = document.getElementById("checkbox-2 checkbox");
 const checkboxThree = document.getElementById("checkbox-3 checkbox");
 const checkboxFour = document.getElementById("checkbox-4 checkbox");
 
+const taskListAdd = document.getElementById("add-button");
+
 let seconds = 00;
 let minutes = 00;
 let secondsBreak = 00;
@@ -24,6 +26,9 @@ let stopWatchTimer;
 let checkbox = 0;
 let running;
 let paused;
+const checkSymbol = "&#10003;";
+const colorRed = "#953B3B";
+let idCount = 00;
 
 startTimer.addEventListener("click", function stopWatch() {
   stopWatchTimer = stopWatch;
@@ -63,7 +68,7 @@ function stopWatchCounter() {
   }
 
   if (minutes >= 25) {
-    timerDisplayMin.innerHTML = 25;
+    timerDisplayMin.innerHTML = "25";
     timerDisplaySec.innerHTML = "00";
     timerBar.style.animation = "load-restart 0s linear forwards";
     secondsBreak += 1;
@@ -92,33 +97,31 @@ function stopWatchCounter() {
     minutes = 00;
     secondsBreak = 00;
     minutesBreak = 00;
-    breakDisplayMin.innerHTML = "05";
-    breakDisplaySec.innerHTML = "00";
-    timerDisplayMin.innerHTML = "25";
-    timerDisplaySec.innerHTML = "00";
+    initialTimer();
     breakBar.style.animation = "load-restart 0s linear forwards";
     checkbox += 1;
 
     if (checkbox === 1) {
-      checkboxOne.innerHTML = "&#10003;";
+      checkboxOne.innerHTML = checkSymbol;
       checkboxOne.style.fontSize = "60px";
-      checkboxOne.style.color = "#953B3B";
+      checkboxOne.style.color = colorRed;
     } else if (checkbox === 2) {
-      checkboxTwo.innerHTML = "&#10003;";
+      checkboxTwo.innerHTML = checkSymbol;
       checkboxTwo.style.fontSize = "60px";
-      checkboxTwo.style.color = "#953B3B";
+      checkboxTwo.style.color = colorRed;
     } else if (checkbox === 3) {
-      checkboxThree.innerHTML = "&#10003;";
+      checkboxThree.innerHTML = checkSymbol;
       checkboxThree.style.fontSize = "60px";
-      checkboxThree.style.color = "#953B3B";
+      checkboxThree.style.color = colorRed;
     } else if (checkbox === 4) {
-      checkboxFour.innerHTML = "&#10003;";
+      checkboxFour.innerHTML = checkSymbol;
       checkboxFour.style.fontSize = "60px";
-      checkboxFour.style.color = "#953B3B";
+      checkboxFour.style.color = colorRed;
       alert("Now take a 15 minutes break. When ready, press restart!");
     }
   }
 }
+
 
 pauseTimer.addEventListener("click", function () {
   if (!paused) {
@@ -135,11 +138,22 @@ restartTimer.addEventListener("click", function () {
   minutes = 00;
   secondsBreak = 00;
   minutesBreak = 00;
-  breakDisplayMin.innerHTML = "05";
-  breakDisplaySec.innerHTML = "00";
-  timerDisplayMin.innerHTML = "25";
-  timerDisplaySec.innerHTML = "00";
   clearInterval(stopWatchFunction);
+  initialTimer();
+  clearBoxes();
+  timerBar.style.animation = "load-restart 0s linear forwards";
+  breakBar.style.animation = "load-restart 0s linear forwards";
+  running = false;
+  paused = true;
+});
+
+taskListAdd.addEventListener("click", function () {
+  taskEvent();
+  
+
+});
+
+function clearBoxes(){
   checkboxOne.innerHTML = "First round!";
   checkboxOne.style.fontSize = "16px";
   checkboxOne.style.color = "black";
@@ -152,8 +166,38 @@ restartTimer.addEventListener("click", function () {
   checkboxFour.innerHTML = "Final round!";
   checkboxFour.style.fontSize = "16px";
   checkboxFour.style.color = "black";
-  timerBar.style.animation = "load-restart 0s linear forwards";
-  breakBar.style.animation = "load-restart 0s linear forwards";
-  running = false;
-  paused = true;
-});
+};
+
+function initialTimer(){
+  breakDisplayMin.innerHTML = "05";
+  breakDisplaySec.innerHTML = "00";
+  timerDisplayMin.innerHTML = "25";
+  timerDisplaySec.innerHTML = "00";
+};
+
+function taskEvent(){
+  const parentTaskDiv = document.getElementById("task-list-div");
+  const taskListInput = document.getElementById("new-task").value;
+  
+  if(taskListInput === ""){
+    alert("You must write a task!")
+  } else {
+  idCount += 1;
+  const newDiv = document.createElement("li"); 
+  const newTask = document.createTextNode(taskListInput);
+  const taskDiv = document.getElementById("task-list-above");
+  taskDiv.appendChild(newDiv);
+  newDiv.appendChild(newTask);
+  // console.log(taskListInput);
+  // console.log(idCount);
+    
+  const span = document.createElement("SPAN");
+  const closeSymbol = document.createTextNode(" x ");
+  span.id = "close-" + idCount;
+  span.appendChild(closeSymbol);
+  newDiv.appendChild(span);
+  // console.log(span);
+    
+  }
+}
+  
